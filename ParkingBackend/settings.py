7b4fc20a570990355a40eae26f8d74b319a372fe
dip_sarker <sarkerdip005@gmail.com>
2025-03-13 00:53:05 +0600

    somechanges

--- ParkingBackend/settings.py
+++ ParkingBackend/settings.py
@@ -65,6 +65,7 @@ CSRF_TRUSTED_ORIGINS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.common.CommonMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
